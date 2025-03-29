4d56abeaa4cd42d9fc52f63958cb408323188f8e
MAHA <mem880.a@gmail.com>
2025-03-29 03:25:04 +0300

    Add: copy right

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='bg-gradient-to-r from-[#3D7EB9] via-[#0DA9A6] to-[#07A869] text-white text-center text-[10px] p-3'>
-            كل الحقوق محفوظة لمدرسة المتوسطة التاسعة والثلاثون
+            كل الحقوق محفوظة لمدرسة المتوسطة التاسعة والثلاثون <br />
+            مبادرة الأستاذة/ هاجر العطوي
         </div>
     )
 }
